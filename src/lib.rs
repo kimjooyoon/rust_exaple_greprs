dd8ff9bc85a168a826e57a89717e42f473215916
kimjooyoon <asketeddy@gmail.com>
2023-01-20 13:57:51 +0900

    test: 대소문자 구문 테스트 추가
    추가로 기존 테스트명 변경

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
@@ -61,6 +61,21 @@ Pick three.";
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
