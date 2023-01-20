01cfe42981d153878f36652d7ff36bcbb5d5adc4
kimjooyoon <asketeddy@gmail.com>
2023-01-20 13:32:06 +0900

    feat: main.rs, lib.rs 분리

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::{env, process};
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
+use ::greprs::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,35 +17,5 @@ fn main() {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)
-        .expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
